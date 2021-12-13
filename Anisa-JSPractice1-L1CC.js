665e42225b4e78914c21441da4f895db81fa88ab
niverred <anisa.zikra@gmail.com>
2021-12-14 00:05:10 +0700

    Create Anisa-JSPractice1-L1CC.js

---
+++ Anisa-JSPractice1-L1CC.js
@@ -0,0 +1,33 @@
+const canGiveBlood = (bloodtypeDonor, bloodtypeRecipient) => {
+
+    let bloodtype = true;
+    let rh = true;
+
+        if (bloodtypeDonor.includes("O")) {
+        bloodtype = true;
+        } 
+            else if (bloodtypeRecipient.includes("AB")) {
+            bloodtype = true;
+            }     
+        else if (bloodtypeDonor[0] === bloodtypeRecipient[0]) {
+        bloodtype = true;
+        } 
+
+    else {
+    bloodtype = false;
+    }
+
+    if (bloodtypeDonor.includes("+") && bloodtypeRecipient.includes("-")) {
+    rh = false;
+    } 
+    
+        else {
+        rh = true;
+        }
+
+  return bloodtype && rh;
+}
+
+console.log(canGiveBlood("O+", "A+"));
+console.log(canGiveBlood("A-", "B-"));
+console.log(canGiveBlood("A-", "AB+"));
